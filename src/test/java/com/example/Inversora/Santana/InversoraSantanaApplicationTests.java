8bae2ead1c43367027b9d6d0bf2e14968a24caaa
Andresan07garzo-hub <an07garzo@gmail.com>
2026-04-17 10:41:12 -0500

    primer commit

---
+++ src/test/java/com/example/Inversora/Santana/InversoraSantanaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Inversora.Santana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InversoraSantanaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
